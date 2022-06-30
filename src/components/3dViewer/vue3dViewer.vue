d6e651cdfc1ddd9f8071f7d04e62d08249d7e398
taoch <chenghong_tao@infinova.com.cn>
2022-06-30 11:03:03 +0800

    init project

---
+++ src/components/3dViewer/vue3dViewer.vue
@@ -0,0 +1,22 @@
+<template></template>
+<script>
+import {
+  Object3D,
+  Vector2,
+  Vector3,
+  Color,
+  Scene,
+  Light,
+  Raycaster,
+  WebGLRenderer,
+  PerspectiveCamera,
+  AmbientLight,
+  PointLight,
+  HemisphereLight,
+  DirectionLight,
+  LinearEncoding,
+  WebGLRendererParameters,
+  Loader,
+} from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+</script>
