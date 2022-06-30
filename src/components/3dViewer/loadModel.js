1a463c98305a2497d600574b20d51983a01b3dec
Tony Tao <king6180@gmail.com>
2022-06-30 21:18:41 +0800

    format files

--- src/components/3dViewer/loadModel.js
+++ src/components/3dViewer/loadModel.js
@@ -1,114 +1,92 @@
-import {
-  Box3,
-  Vector3,
-  Mesh,
-  MeshPhongMaterial,
-  MeshStandardMaterial
-} from 'three'
-import {
-  FBXLoader
-} from 'three/examples/jsm/loaders/FBXLoader'
-import {
-  ColladaLoader
-} from 'three/examples/jsm/loaders/ColladaLoader'
-import {
-  GLTFLoader
-} from 'three/examples/jsm/loaders/GLTFLoader'
-import {
-  OBJLoader
-} from 'three/examples/jsm/loaders/OBJLoader'
-import {
-  DDSLoader
-} from 'three/examples/jsm/loaders/DDSLoader'
-import {
-  LoadingManager
-} from 'three/src/loaders/LoadingManager'
-import {
-  PLYLoader
-} from 'three/examples/jsm/loaders/PLYLoader'
-import {
-  STLLoader
-} from 'three/examples/jsm/loaders/STLLoader'
+import { Box3, Vector3, Mesh, MeshPhongMaterial, MeshStandardMaterial } from "three"
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
+import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
+import { DDSLoader } from "three/examples/jsm/loaders/DDSLoader"
+import { LoadingManager } from "three/src/loaders/LoadingManager"
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader"
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader"
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader"
 
-const box = new Box3()
-const manager = (new LoadingManager())
-manager.addHandler(/\.dds$/i, new DDSLoader())
+const box = new Box3();
+const manager = (new LoadingManager());
+manager.addHandler(/\.dds$/i, new DDSLoader());
 
 // get box size
 function getSize(obj) {
-  box.setFromObject(obj)
-  return box.getSize(new Vector3())
+  box.setFromObject(obj);
+  return box.getSize(new Vector3());
 }
 
 // get box center
 function getCenter(obj) {
-  box.setFromObject(obj)
-  return box.getCenter(new Vector3())
+  box.setFromObject(obj);
+  return box.getCenter(new Vector3());
 }
 
 // auto select model loader
 function getLoader(filePath) {
   // Get file extension
-  let fileExtension = filePath.split('.').filter(i => i)[1]
+  let fileExtension = filePath.split(".").filter(i => i)[1];
   // gltf type has two formats, .gltf and .glb, so make fileExtension glb to gltf
-  if (fileExtension === 'glb') {
-    fileExtension = 'gltf'
+  if (fileExtension === "glb") {
+    fileExtension = "gltf";
   }
   let obj = {
     loader: null,
     getObject: null
-  } // obj {loader, getObject}
+  }; // obj {loader, getObject}
   switch (fileExtension) {
-    case 'dae':
+    case "dae":
       obj = {
         loader: new ColladaLoader(),
         getObject: (collada) => {
-          return collada.scene
+          return collada.scene;
         }
-      }
-      break
-    case 'fbx':
+      };
+      break;
+    case "fbx":
       obj = {
         loader: new FBXLoader()
-      }
-      break
-      // gltf
-    case 'gltf':
+      };
+      break;
+    // gltf
+    case "gltf":
       obj = {
         loader: new GLTFLoader()
-      }
-      break
-    case 'obj':
+      };
+      break;
+    case "obj":
       obj = {
         loader: new OBJLoader(manager),
         getObject: null
-      }
-      break
-    case 'ply':
+      };
+      break;
+    case "ply":
       obj = {
         loader: new PLYLoader(),
         getObject: (geometry) => { // geometry
-          geometry.computeVertexNormals()
-          return new Mesh(geometry, new MeshStandardMaterial())
+          geometry.computeVertexNormals();
+          return new Mesh(geometry, new MeshStandardMaterial());
         }
-      }
-      break
-    case 'stl':
+      };
+      break;
+    case "stl":
       obj = {
         loader: new STLLoader(),
         getObject: (geometry) => { // geometry
-          return new Mesh(geometry, new MeshPhongMaterial())
+          return new Mesh(geometry, new MeshPhongMaterial());
         }
-      }
-      break
+      };
+      break;
   }
-  return obj
+  return obj;
 }
 
 function getMTLLoader() {
-  const mtlLoader = new MTLLoader(manager)
-  return mtlLoader
+  const mtlLoader = new MTLLoader(manager);
+  return mtlLoader;
 }
 
 export {
