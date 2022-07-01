9cf50a28836a0483e5be0a5804a231a1421532ac
Tony Tao <king6180@gmail.com>
2022-07-02 00:03:49 +0800

    add example load-a-model

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('./src'))
+      .set('@assets', resolve('./src/assets'))
+  },
 })
