f17aaae9b12ad064c2f35f42682fa107dfd4ba0e
Tony Tao <king6180@gmail.com>
2022-07-02 00:03:49 +0800

    add example load-a-model

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@ html,
 body {
   margin: 0;
   padding: 0;
+  width: 100%;
+  height: 100%;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -27,5 +29,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/examples/exampleIndex.vue
+++ src/examples/exampleIndex.vue
@@ -1,40 +1,72 @@
 <template>
-  <div>
+  <div class="example">
     <div class="menu">
       <ul>
-        <li v-for="item in menu" :key="item.id">{{ item.name }}</li>
+        <li
+          :class="currentMenuId === item.id ? 'active' : ''"
+          v-for="item in menu"
+          :key="item.id"
+          @click="getMenuId(item.id)"
+        >
+          {{ getCurrentLanguageName(item.id) }}
+        </li>
       </ul>
     </div>
-    <loaderFilePathVue />
+    <loadAModelVue v-if="currentMenuId === 1" />
+    <!-- <loaderFilePathVue /> -->
   </div>
 </template>
 <script>
-import loaderFilePathVue from "./loaderFilePath.vue";
+// import loaderFilePathVue from "./loaderFilePath.vue";
+import loadAModelVue from "./load-a-model.vue";
 export default {
+  components: {
+    // loaderFilePathVue,
+    loadAModelVue,
+  },
   data() {
     return {
       menu: [
-        { id: 1, name: "加载一个模型" },
-        { id: 2, name: "加载多个模型" },
-        { id: 3, name: "设置场景高宽" },
-        { id: 4, name: "材质及纹理" },
-        { id: 5, name: "背景颜色及透明度" },
-        { id: 6, name: "交互控制" },
-        { id: 7, name: "旋转模型" },
-        { id: 8, name: "事件" },
-        { id: 9, name: "灯光" },
-        { id: 10, name: "显示FPS信息" },
-        { id: 11, name: "相机位置及旋转" },
-        { id: 12, name: "加载进度" },
+        { id: 1, name_CN: "加载一个模型", name_EN: "Load a model" },
+        { id: 2, name_CN: "加载多个模型", name_EN: "Load multiple models" },
+        { id: 3, name_CN: "设置场景高宽", name_EN: "Height and width" },
+        { id: 4, name_CN: "材质及纹理", name_EN: "Materials and textures" },
+        { id: 5, name_CN: "背景颜色及透明度", name_EN: "Background" },
+        { id: 6, name_CN: "交互控制", name_EN: "Control" },
+        { id: 7, name_CN: "旋转模型", name_EN: "Rotary model" },
+        { id: 8, name_CN: "事件", name_EN: "Event" },
+        { id: 9, name_CN: "灯光", name_EN: "Light" },
+        { id: 10, name_CN: "显示FPS信息", name_EN: "Show stats" },
+        { id: 11, name_CN: "相机位置及旋转", name_EN: "Camera" },
+        { id: 12, name_CN: "加载进度", name_EN: "Progress bar" },
       ],
+      currentMenuId: 1,
     };
   },
-  components: {
-    loaderFilePathVue,
+  methods: {
+    getLocal() {
+      let lang = navigator.language.substring(0, 2) || "zh";
+      return lang;
+    },
+    getCurrentLanguageName(id) {
+      let menuItem = this.menu.filter((item) => item.id === id)[0];
+      let lang = this.getLocal();
+      if (lang === "zh") {
+        return menuItem.name_CN;
+      } else {
+        return menuItem.name_EN;
+      }
+    },
+    getMenuId(id) {
+      this.currentMenuId = id;
+    },
   },
 };
 </script>
 <style>
+.example {
+  height: 100%;
+}
 ul,
 li {
   list-style-type: none;
@@ -44,8 +76,13 @@ li {
 li {
   padding: 5px 10px;
 }
+li.active {
+  background-color: rgb(88, 88, 88);
+  text-decoration: underline;
+}
 li:hover {
   background-color: rgb(88, 88, 88);
+  text-decoration: underline;
 }
 .menu {
   position: absolute;
