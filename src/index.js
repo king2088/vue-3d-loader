1d211af793ae542c08921108088b54011dd48ebb
king2088 <king6180@gmail.com>
2022-07-01 18:01:55 +0800

    add install plugin

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import vue3dLoader from './3dLoader/vue3dLoader.vue'
+const components = [vue3dLoader]
+
+// vue install function
+const install = function (Vue) {
+  // Register components
+  components.map(component =>{
+    Vue.component(component.name, component)
+  })
+}
+
+if(typeof window !== 'undefined' && window.Vue){
+  install(window.Vue)
+}
+
+export default {
+  install,
+  ...components
+}
