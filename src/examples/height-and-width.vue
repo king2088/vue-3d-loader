930a83707818ba66e294ce34e0f61c16e1864f53
Tony Tao <king6180@gmail.com>
2022-07-02 18:07:32 +0800

    fix filepath and update readme

--- src/examples/height-and-width.vue
+++ src/examples/height-and-width.vue
@@ -1,7 +1,7 @@
 <template>
   <vue3dLoader
-    :filePath="'models/gltf/DamagedHelmet.gltf'"
-    :backgroundColor="'#00ffff'"
+    filePath="/models/gltf/DamagedHelmet.gltf"
+    backgroundColor="#00ffff"
     :height="600"
     :width="600"
   ></vue3dLoader>
--- src/examples/load-a-model.vue
+++ src/examples/load-a-model.vue
@@ -1,6 +1,6 @@
 <template>
   <vue3dLoader
-    :filePath="'models/collada/stormtrooper/stormtrooper.dae'"
+    filePath="/models/collada/stormtrooper/stormtrooper.dae"
     :scale="{ x: 0.2, y: 0.2, z: 0.2 }"
   ></vue3dLoader>
 </template>
