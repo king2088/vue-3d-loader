7a6890ee559f3f7df2a86444983a1c23d047a953
Tony Tao <king6180@gmail.com>
2022-07-10 20:48:00 +0800

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require('path');
 
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'examples-demo'
