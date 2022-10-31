e640768e979d8d16fcfb3f1b20405a50efec1dd6
Tony Tao <king6180@gmail.com>
2022-10-31 20:38:50 +0800

    update draco url

--- assets/LoadJsonModel.c4de084e.js
+++
@@ -1,1 +0,0 @@
-import{v as o}from"./vue-3d-loader.esm.01dc0c17.js";import{f as e,o as a,q as s,h as n,_ as r}from"../app.17a07919.mjs";const t=e({__name:"LoadJsonModel",setup(d){return(_,c)=>(a(),s(n(o),{filePath:"/vue-3d-loader-docs/models/json/lightmap.json",height:350,backgroundColor:"#F2F2F2"}))}}),p=r(t,[["__file","LoadJsonModel.vue"]]);export{p as default};
