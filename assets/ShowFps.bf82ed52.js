bdde2602f273fef0878f51c5b1ccb48b167db68f
Tony Tao <king6180@gmail.com>
2022-11-04 23:11:01 +0800

    update docs

---
+++ assets/ShowFps.bf82ed52.js
@@ -0,0 +1,1 @@
+import{v as o}from"./vue-3d-loader.esm.3baf85bb.js";import{f as e,o as s,q as r,h as t,_ as a}from"../app.7f2ee038.mjs";const p=e({__name:"ShowFps",setup(_){return(c,n)=>(s(),r(t(o),{filePath:"/vue-3d-loader-docs/models/obj/tree.obj",height:350,backgroundColor:"#F2F2F2",showFps:!0}))}}),d=a(p,[["__file","ShowFps.vue"]]);export{d as default};
