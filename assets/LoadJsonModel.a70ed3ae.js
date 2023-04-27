ba590b82f0cff9cd614800f5a7b02bf994f8a764
Tony Tao <king6180@gmail.com>
2023-04-27 20:51:41 +0800

    update docs

---
+++ assets/LoadJsonModel.a70ed3ae.js
@@ -0,0 +1,1 @@
+import{v as o}from"./vue-3d-loader.esm.22a65b72.js";import{f as e,o as a,q as s,h as n,_ as r}from"../app.04b21cbc.mjs";const t=e({__name:"LoadJsonModel",setup(d){return(_,c)=>(a(),s(n(o),{filePath:"/vue-3d-loader-docs/models/json/lightmap.json",height:350,backgroundColor:"#F2F2F2"}))}}),p=r(t,[["__file","LoadJsonModel.vue"]]);export{p as default};
