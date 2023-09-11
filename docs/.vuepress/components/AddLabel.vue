a74672183d2ce01fbfc418ceeb16ea48c0cfd837
king2088 <king6180@gmail.com>
2023-09-11 16:36:26 +0800

    update labels demo

--- docs/.vuepress/components/AddLabel.vue
+++ docs/.vuepress/components/AddLabel.vue
@@ -1,12 +1,19 @@
 <template>
-  <vue3dLoader
-    filePath="/vue-3d-loader-docs/models/gltf/DamagedHelmet.gltf"
-    :height="350"
-    backgroundColor="#F2F2F2"
-    :labels="labels"
-    :scale="{ x: 0.5, y: 0.5, z: 0.5 }"
-    outputEncoding="sRGB"
-  />
+  <div class="top">
+    <button @click="changeLabelPosition">Change label position</button>
+    <button @click="changeTextLabelName">Change text label name</button>
+    <button @click="resetLabels">Reset labels</button>
+  </div>
+  <div class="content">
+    <vue3dLoader
+      filePath="/vue-3d-loader-docs/models/gltf/DamagedHelmet.gltf"
+      backgroundColor="#cccccc"
+      :labels="labels"
+      :scale="{ x: 0.5, y: 0.5, z: 0.5 }"
+      outputEncoding="sRGB"
+      :height="500"
+    />
+  </div>
 </template>
 <script setup lang="ts">
 import { vue3dLoader } from "vue-3d-loader";
@@ -45,4 +52,41 @@ labels.value = [
     sid: 3, // 自定义标识，可有可无
   },
 ];
+
+function changeLabelPosition() {
+  labels.value[0].position = { x: -0.2, y: 2, z: 1 }
+}
+
+function changeTextLabelName() {
+  labels.value[2].text = "My name is change"
+}
+
+function resetLabels() {
+  labels.value[0].position = { x: -0.5, y: 1, z: 0 }
+  labels.value[2].text = "I'm Text Label"
+}
 </script>
+<style>
+.top{
+  position: relative;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-bottom: 8px;
+}
+.top button{
+  margin: 0 2px;
+  background: rgb(43, 109, 202);
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  padding: 4px 6px;
+}
+.top button:hover{
+  background: rgb(27, 62, 186);
+}
+.content{
+  width: 100%;
+  height: 500px;
+}
+</style>
