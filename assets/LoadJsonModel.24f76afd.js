db367d8cfd13b503623ffc6c62921b6c0b972155
king2088 <king6180@gmail.com>
2023-11-01 23:37:26 +0800

    update deploy

---
+++ assets/LoadJsonModel.24f76afd.js
@@ -0,0 +1,1 @@
+import{v as o}from"./vue-3d-loader.esm.d85925bb.js";import{f as e,o as a,q as s,h as n,_ as r}from"../app.55b22865.mjs";const t=e({__name:"LoadJsonModel",setup(d){return(_,c)=>(a(),s(n(o),{filePath:"/vue-3d-loader-docs/models/json/lightmap.json",height:350,backgroundColor:"#F2F2F2"}))}}),p=r(t,[["__file","LoadJsonModel.vue"]]);export{p as default};
